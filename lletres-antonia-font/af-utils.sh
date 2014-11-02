958f287a0acdf532e99cbb2da49fbaa422c59a57
Pep <pepmanuel@gmail.com>
2014-11-02 04:04:23 +0100

    Afegida una nova estadística (número de cançons on apareix cada paraula). Afegits els resultats.

---
+++ lletres-antonia-font/af-utils.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+URL_VIASONA="http://www.viasona.cat/grup/antonia-font/totes-les-lletres"
+
+function corregir_text() {
+	# Copiar stdin a un fitxer temporal.
+	fitxer_tmp=/tmp/$RANDOM.tmp
+	cat /dev/stdin > $fitxer_tmp
+
+	# Eliminar espais a començament de línia.
+	sed -i -r "s/^( +)//g" $fitxer_tmp
+	# Eliminar <br/>.
+	sed -i "s/<br\/>//g" $fitxer_tmp
+	# Reemplaçar &#39; per '.
+	sed -i "s/&#39;/'/g" $fitxer_tmp
+	# Reemplaçar ´ per '.
+	sed -i "s/´/'/g" $fitxer_tmp
+	# Eliminar símbol no imprimible.
+	sed -i "s///g" $fitxer_tmp
+	# Reemplaçar caràcter estrany (no és un espai) per un espai.
+	sed -i "s/ / /g" $fitxer_tmp
+	# Eliminar cometes simples repetides.
+	sed -i "s/''/ \"/g" $fitxer_tmp
+	# Eliminar espais repetits
+	sed -i -r "s/( +)/ /g" $fitxer_tmp
+	# Assegurar que hi ha un espai darrera cada punt (en fer el recompte de
+	# paraules seran eliminants).
+	# Excepcions: quan estan entre números (30.000), quan formen part de 
+	# sigles (S.O.S.) i quan són punts suspensius (...).
+	sed -i -r "s/([a-z]+)\.([A-Z]+)/\1\. \2/g" $fitxer_tmp
+	# Assegurar que hi ha un espai darrera els signes d'interrogació
+	# (ja que posteriorment seran eliminats).
+	sed -i "s/\?/\? /g" $fitxer_tmp
+
+	# Imprimir a stdout.
+	cat $fitxer_tmp
+	rm $fitxer_tmp
+}
+
+function baixar_lletres() {
+	# Obtenir els enllaços a les lletres de les cançons.
+	wget -O- $URL_VIASONA | hxnormalize -x | hxselect -i "div.titol > a" | \
+	sed -r "s/href/\nhref/g" | grep href | \
+	sed -r "s/href=\"([^\"]+)([^$]+)/\1/g" > resultats/urls-lletres-af.txt
+
+	# Baixar les pàgines HTML de totes les cançons.
+	mkdir resultats/pagines-html-lletres
+	wget -P resultats/pagines-html-lletres -i resultats/urls-lletres-af.txt
+
+	# Extreure les lletres de les pàgines HTML.
+	mkdir resultats/lletres
+	cd resultats/pagines-html-lletres
+	for f in `ls`
+	do
+		hxnormalize -x $f | \
+		hxselect -c -i "p.contingut_lletra" | corregir_text > ../lletres/$f
+	done
+
+	cd ../..
+}
+
+function processar_text() {
+	# Copiar stdin a un fitxer temporal.
+	fitxer_tmp=/tmp/$RANDOM.tmp
+	cat /dev/stdin > $fitxer_tmp
+
+	# Eliminar caràcters especials.
+	sed -i "s/[,.;:\¿\?\!\(\)\"]//g" $fitxer_tmp
+	# Eliminar guions a començament de línia.
+	sed -i "s/^\-//g" $fitxer_tmp
+	# Convertir-ho tot a minúscules.
+	sed -i -e "s/./\L&/g" $fitxer_tmp
+	# Eliminar partícules apostrofades al començament de paraules.
+	sed -i -r "s/(d')|(l')|(m')|(n')|(s')|(t')//g" $fitxer_tmp
+	# Reemplaçar espais per salts de línia (per aplicar uniq posteriorment).
+	sed -i "s/ /\n/g" $fitxer_tmp
+	# Eliminar línies en blanc.
+	sed -i "/^$/d" $fitxer_tmp
+	
+	# Imprimir a stdout.
+	cat $fitxer_tmp
+	rm $fitxer_tmp
+}
--- lletres-antonia-font/af.sh
+++ lletres-antonia-font/af.sh
@@ -1,61 +1,30 @@
 #!/bin/bash
 
-# Pàgina amb enllaços a totes les cançons d'Antònia Font
-URL_VIASONA="http://www.viasona.cat/grup/antonia-font/totes-les-lletres"
+source af-utils.sh
 
-# Obtenir els enllaços a les lletres de les cançons
-wget -O- $URL_VIASONA | hxnormalize -x | hxselect -i "div.titol > a" | \
-sed -r "s/href/\nhref/g" | grep href | \
-sed -r "s/href=\"([^\"]+)([^$]+)/\1/g" > urls-lletres-af.txt
+if [ ! -d "resultats" ]; then
+	mkdir resultats
+fi
 
-mkdir resultat
-mkdir pagines-cançons
-cd pagines-cançons
+baixar_lletres
 
-# Davallar les pàgines HTML de totes les cançons
-cat ../urls-lletres-af.txt | wget -O- -i-
+cd resultats/lletres/
 
-# Extreure les lletres de les pàgines HTML i posar-les en un fitxer
-# "ls --ignore *.?" llista tots els noms de fitxers que no acaben amb
-# punt seguit d'un número (cançons repetides: alegria.1, alegria.2, ...).
-ls --ignore "*.?" | xargs cat | hxnormalize -x | \
-hxselect -c -i "p.contingut_lletra" > ../resultat/lletres.txt
+mkdir ../resultat1
+# "ls --ignore=*.?" llista tots els noms de fitxers que no acaben amb
+# punt seguit d'un número (cançons repetides en diferents discs: alegria.1, alegria.2, ...).
+# A continuació es posen totes les lletres dins un únic fitxer.
+ls --ignore=*.? | xargs cat | processar_text > ../resultat1/lletres_processades.txt
+# Recompte.
+printf "Número de vedades que la paraula apareix a les lletres d'Antònia Font:\n\n" > ../resultat1/frequencies.txt
+cat ../resultat1/lletres_processades.txt | sort | uniq -c | sort -nr >> ../resultat1/frequencies.txt
 
-cd ../resultat
-cp lletres.txt lletres_tmp.txt
-
-# Eliminar espais a començament de línia
-sed -i -r "s/^( +)//g" lletres_tmp.txt
-# Eliminar <br/>
-sed -i "s/<br\/>//g" lletres_tmp.txt
-# Reemplaçar &#39; per '
-sed -i "s/&#39;/'/g" lletres_tmp.txt
-# Assegurar que hi ha espais darrera els punts (abans d'eliminar-los)
-# Excepcions: quan estan entre números (30.000), quan formen part de 
-# sigles (S.O.S.) i quan són punts suspensius (...).
-sed -i -r "s/([a-z]+)\.([A-Z]+)/\1\. \2/g" lletres_tmp.txt
-# Assegurar que hi ha un espai darrera els signes d'interrogació (abans d'eliminar-los)
-sed -i "s/\?/\? /g" lletres_tmp.txt
-# Eliminar caràcters especials
-sed -i "s/[,.;:\¿\?\!\(\)\"]//g" lletres_tmp.txt
-# Eliminar guió a començament de línia
-sed -i "s/^\-//g" lletres_tmp.txt
-# Reemplaçar ´ per '
-sed -i "s/´/'/g" lletres_tmp.txt
-# Eliminar símbol no imprimible
-sed -i "s///g" lletres_tmp.txt
-# Reemplaçar caràcter estrany (no és un espai) per un espai
-sed -i "s/ / /g" lletres_tmp.txt
-# Eliminar cometes simples repetides
-sed -i "s/''/ /g" lletres_tmp.txt
-# Eliminar múltiples espais
-sed -i -r "s/( +)/ /g" lletres_tmp.txt
-# Convertir-ho tot a minúscules
-dd if=lletres_tmp.txt of=lletres_processades.txt conv=lcase
-# Eliminar partícules apostrofades al començament de paraules
-sed -i -r "s/(d')|(l')|(m')|(n')|(s')|(t')//g" lletres_processades.txt
-
-rm lletres_tmp.txt
-
-# Obtenir el llistat de freqüències de paraules per ordre decreixent
-cat lletres_processades.txt | tr ' ' '\n' | sort | uniq -c | sort -nr > frequencies.txt
+mkdir ../resultat2
+# S'obtenen les paraules úniques de cada cançó i es posen dins un únic fitxer.
+for f in `ls --ignore=*.?`
+do
+	cat $f | processar_text | sort | uniq >> ../resultat2/lletres_processades.txt
+done
+# Recompte.
+printf "Número de cançons d'Antònia Font on apareix la paraula:\n\n" > ../resultat2/frequencies.txt
+cat ../resultat2/lletres_processades.txt | sort | uniq -c | sort -nr >> ../resultat2/frequencies.txt
